9d949c8346624f7e0751981d3a4e330836641281
aresyang <yangrui.li@mobvista.com>
2019-09-24 15:52:12 +0800

    format

--- recycle.c
+++ recycle.c
@@ -14,72 +14,63 @@ This also decreases memory fragmentation, and freeing structures
 */
 
 #ifndef STANDARD
-# include "standard.h"
+#include "standard.h"
 #endif
 #ifndef RECYCLE
-# include "recycle.h"
+#include "recycle.h"
 #endif
 #include <stdlib.h>
 #include <string.h>
-reroot *remkroot(size_t  size)
-{
-   reroot *r = (reroot *)remalloc(sizeof(reroot), "recycle.c, root");
-   r->list = (recycle *)0;
-   r->trash = (recycle *)0;
-   r->size = align(size);
-   r->logsize = RESTART;
-   r->numleft = 0;
-   return r;
+reroot* remkroot(size_t size) {
+    reroot* r = (reroot*)remalloc(sizeof(reroot), "recycle.c, root");
+    r->list = (recycle*)0;
+    r->trash = (recycle*)0;
+    r->size = align(size);
+    r->logsize = RESTART;
+    r->numleft = 0;
+    return r;
 }
 
-void  refree(struct reroot *r)
-{
-   recycle *temp;
-   if (NULL != (temp = r->list)) {
-       while (r->list)
-       {
-          temp = r->list->next;
-          free((char *)r->list);
-          r->list = temp;
-       }
-   }
-   free((char *)r);
-   return;
+void refree(struct reroot* r) {
+    recycle* temp;
+    if (NULL != (temp = r->list)) {
+        while (r->list) {
+            temp = r->list->next;
+            free((char*)r->list);
+            r->list = temp;
+        }
+    }
+    free((char*)r);
+    return;
 }
 
 /* to be called from the macro renew only */
-char  *renewx(struct reroot *r)
-{
-   recycle *temp;
-   if (r->trash)
-   {  /* pull a node off the trash heap */
-      temp = r->trash;
-      r->trash = temp->next;
-      (void)memset((void *)temp, 0, r->size);
-   }
-   else
-   {  /* allocate a new block of nodes */
-      r->numleft = r->size*((ub4)1<<r->logsize);
-      if (r->numleft < REMAX) ++r->logsize;
-      temp = (recycle *)remalloc(sizeof(recycle) + r->numleft, 
-				 "recycle.c, data");
-      temp->next = r->list;
-      r->list = temp;
-      r->numleft-=r->size;
-      temp = (recycle *)((char *)(r->list+1)+r->numleft);
-   }
-   return (char *)temp;
+char* renewx(struct reroot* r) {
+    recycle* temp;
+    if (r->trash) { /* pull a node off the trash heap */
+        temp = r->trash;
+        r->trash = temp->next;
+        (void)memset((void*)temp, 0, r->size);
+    }
+    else { /* allocate a new block of nodes */
+        r->numleft = r->size * ((ub4)1 << r->logsize);
+        if (r->numleft < REMAX)
+            ++r->logsize;
+        temp =
+            (recycle*)remalloc(sizeof(recycle) + r->numleft, "recycle.c, data");
+        temp->next = r->list;
+        r->list = temp;
+        r->numleft -= r->size;
+        temp = (recycle*)((char*)(r->list + 1) + r->numleft);
+    }
+    return (char*)temp;
 }
 
-char   *remalloc(size_t  len,char   *purpose)
-{
-  char *x = (char *)malloc(len);
-  if (!x)
-  {
-    fprintf(stderr, "malloc of %lu failed for %s\n", 
-	    len, purpose);
-    exit(SUCCESS);
-  }
-  return x;
+char* remalloc(size_t len, char* purpose) {
+    char* x = (char*)malloc(len);
+    if (!x) {
+        fprintf(stderr, "malloc of %lu failed for %s\n", len, purpose);
+        exit(SUCCESS);
+    }
+    return x;
 }
-
